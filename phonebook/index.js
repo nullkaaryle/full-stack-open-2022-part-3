0ac9f851e877d9de95ad3d6bc91664d5886c7550
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2022-06-30 16:48:24 +0300

    add part 3 exercise 3.3: phonebook backend, step 3

--- phonebook/index.js
+++ phonebook/index.js
@@ -27,20 +27,38 @@ let persons = [
 const numberOfPersons = persons.length
 const timeNow = Date()
 
-const infotext =`<p>Phonebook has info for ${numberOfPersons} people</p> <p>${timeNow}</p>`
-    
+const infotext = `<p>Phonebook has info for ${numberOfPersons} people</p> <p>${timeNow}</p>`
 
+const persontext = (person) => {
+    return (`
+        <h3>Contact from the phonebook</h3>
+        <p><b>Contact name:</b> ${person.name}</p> 
+        <p><b>Contact phonenumber:</b> ${person.number}</p>
+        <p><b>Phonebook id:</b> ${person.id}</p>
+        `)
+}
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello Phonebook!</h1>')
+app.get('/', (request, response) => {
+    response.send('<h1>Hello Phonebook!</h1>')
 })
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/info', (request, response) => {
+    response.send(infotext)
 })
 
-app.get('/info', (req, res) => {
-    res.send(infotext)
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.send(persontext(person))
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
