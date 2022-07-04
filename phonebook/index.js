1f08bd1b01787f691c03e89d0a24fdcb18502f74
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2022-07-04 22:33:21 +0300

    add part 3 exercise 3.9: phonebook backend, step 9

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { token } = require('morgan')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use(morgan(
     'METHOD: :method - URL: :url - STATUS: :status - RESPONSE TIME: :response-time[3] ms - POSTED DATA: :postData'
@@ -118,7 +120,7 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
