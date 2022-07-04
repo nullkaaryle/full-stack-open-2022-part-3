dfcc4c2d8d6fdcadf223b3cf0bbd5016990dfc87
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2022-07-04 23:30:59 +0300

    add part 3 exercise 3.11: phonebook full stack

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 app.use(morgan(
     'METHOD: :method - URL: :url - STATUS: :status - RESPONSE TIME: :response-time[3] ms - POSTED DATA: :postData'
